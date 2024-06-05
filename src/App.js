9ca2bd37f4634df70c6379af841de9297b4317af
soorajain <soorajjain51@gmail.com>
2024-06-05 11:25:27 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import React from "react";
+import Layout from "./Layout";
+import Blogs from "./Blogs";
+import Contact from "./Contact";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/Blogs" element={<Blogs />}></Route>
+            <Route path="/Contact" element={<Contact />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/Layout.js
+++ src/Layout.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Layout() {
   return (
     <div>
-      
+      <nav>
+        <ul>
+          <li>
+            <Link to="/"> Home </Link>
+          </li>
+          <li>
+            <Link to="/Blogs"> Blogs </Link>
+          </li>
+          <li>
+            <Link to="/Contact"> Contact </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
